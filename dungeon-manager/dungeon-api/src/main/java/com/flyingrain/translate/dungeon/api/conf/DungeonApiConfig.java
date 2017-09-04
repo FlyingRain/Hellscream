ed627294cf5183f83e4d11dc2fb9716bdf51701d
FlyingRain <1198420279@qq.com>
2017-09-04 15:54:38 +0800

    add config

---
+++ dungeon-manager/dungeon-api/src/main/java/com/flyingrain/translate/dungeon/api/conf/DungeonApiConfig.java
@@ -0,0 +1,10 @@
+package com.flyingrain.translate.dungeon.api.conf;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wally on 8/16/17.
+ */
+@Configuration
+public class DungeonApiConfig {
+}
